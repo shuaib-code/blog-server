31e88893545cab0c874d9af71ebd132835042629
shuaib-code <shuaib.cyclist@gmail.com>
2024-12-19 23:51:40 +0600

    build script

--- src/app/modules/admin/admin.ts
+++ src/app/modules/admin/admin.ts
@@ -1,7 +1,5 @@
-export * from "./admin.constant";
 export * from "./admin.controller";
 export * from "./admin.interface";
 export * from "./admin.model";
 export * from "./admin.routes";
-export * from "./admin.schema";
 export * from "./admin.service";
